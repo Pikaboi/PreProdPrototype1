dc89c23b8bef25919e5d3e406a9871359f840d3e
Pikaboi <braden.cleur@gmail.com>
2021-07-28 14:39:05 +1200

    so far

--- Assets/Scripts/FPSCam.cs
+++ Assets/Scripts/FPSCam.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class FPSCam : MonoBehaviour
 {
+    [SerializeField] private GameObject m_follow;
+    [SerializeField] private Vector3 m_offset;
+    [SerializeField] private float m_sensitivity;
+
+    [SerializeField] private float m_rotationX = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.transform.position = m_follow.transform.position + m_offset;
+    }
+
+    void Update()
+    {
+        //Camera Rotates up and down
+        float vert = Input.GetAxisRaw("Mouse Y") * m_sensitivity * Time.deltaTime;
+
+        m_rotationX -= vert;
+
+        m_rotationX = Mathf.Clamp(m_rotationX, -25.0f, 25.0f);
+
+        transform.localRotation = Quaternion.Euler(m_rotationX, 0.0f, 0.0f);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        //Pretty simple since we are going very combat heavy
+        gameObject.transform.position = m_follow.transform.position + m_offset;
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //Usual Stuff
+    [SerializeField] private Rigidbody m_rb;
+    [SerializeField] private float m_Speed;
+    [SerializeField] private float m_Sensitivity;
+
+    private float m_RotationY;
+
+    private Vector3 m_direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Schmoving
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Mouse X") * m_Sensitivity * Time.deltaTime;
+
+        //To connect it too the velocity
+        Vector3 m_movement = new Vector3(x, 0.0f, z).normalized;
+
+        m_RotationY -= horizontal;
+
+        transform.localRotation = Quaternion.Euler(new Vector3(0.0f, m_RotationY, 0.0f));
+
+        //So we keep gravity
+        m_rb.velocity = new Vector3((m_movement * m_Speed).x, m_rb.velocity.y, (m_movement * m_Speed).z);
     }
 }
