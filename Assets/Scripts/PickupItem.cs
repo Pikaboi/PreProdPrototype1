90d049c3338ad5d0fc04636bd773cf49da6f0ce3
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-08-02 15:04:16 +1200

    pick up item start

---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    virtual public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    virtual public void Update()
+    {
+        
+    }
+
+    virtual public void Interact()
+    {
+        Debug.Log("Interaction");
+    }
+}
