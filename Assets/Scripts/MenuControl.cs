984306231e0bc0ac406e600f040de1189634ce80
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-08-02 16:39:04 +1200

    unlocks on menu

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void QuitGame()
     {
         Application.Quit();
