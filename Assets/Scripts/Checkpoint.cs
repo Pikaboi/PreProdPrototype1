50d0e41e578612525ed3b152e493f54d66dd2044
Pikaboi <braden.cleur@gmail.com>
2021-08-06 15:34:14 +1200

    Revert "checkpoint"
    This reverts commit 3a77b4137dbd6d68a95444d5589f7ad433e4cd90.

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,55 +4,15 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    [SerializeField] List<Enemy> enemies;
-    List<Enemy> enemycopies;
-
-    [SerializeField] bool m_Activated = false;
     // Start is called before the first frame update
     void Start()
     {
-        Enemy[] collection = GameObject.FindObjectsOfType<Enemy>();
-        foreach(Enemy col in collection)
-        {
-            enemies.Add(col);
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            if (m_Activated == false)
-            {
-                SaveUpdates();
-            }
-        }
-    }
-
-    void SaveUpdates()
-    {
-        foreach (Enemy e in enemies)
-        {
-            if(e == null)
-            {
-                enemies.Remove(e);
-            }
-        }
-
-        m_Activated = true;
-    }
-
-    public void ResetWorld()
-    {
-        foreach(Enemy e in enemies)
-        {
-
-        }
     }
 }
